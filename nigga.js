39d811f56998cacbdfe35714a95ed1a6da380a6e
faso <83717613+2gmh@users.noreply.github.com>
2023-02-10 23:19:23 -0300

    Delete nigga.js

--- nigga.js
+++
@@ -1,7 +0,0 @@
-let titles = ['♱| ...',' ♱| 2g..',' ♱| m.♱','♱|h♱']
-let index = 0
-let interval = setInterval(() => {
-    document.title = titles[index]
-    index += 1
-    if (index === titles.length) index = 0
-}, 500)
